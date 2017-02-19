4d81f542745e38d04d57a4a55effd382bac37b94
Luk Ming Kit <luk_mk@hotmail.com>
2017-02-19 19:45:39 +0800

    Added javadoc to SuccessCollator

--- app/src/main/java/com/github/lmk1988/collator/SuccessCollator.java
+++ app/src/main/java/com/github/lmk1988/collator/SuccessCollator.java
@@ -4,7 +4,17 @@ import android.support.annotation.Nullable;
 
 import java.util.List;
 
+/**
+ * A template for {@link CallbackCollator} to collate async success/failure results
+ */
 public class SuccessCollator extends CallbackCollator<Boolean> {
+
+    /**
+     * Checks if the given list of boolean are all true
+     *
+     * @param collatedSuccess
+     * @return true when collatedSuccess is not null and are all true
+     */
     public static boolean isAllSuccess(@Nullable List<Boolean> collatedSuccess) {
         if (collatedSuccess == null) {
             return false;
